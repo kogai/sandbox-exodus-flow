ede25c787030fcfced6b4bfb0feb39cfc7e85b5f
Shinichi Kogai <kogai0121@gmail.com>
2018-06-02 13:29:24 +0900

    fix typo

--- src/exodus.ml
+++ src/exodus.ml
@@ -9,6 +9,6 @@ export const f: string => number = (x) => {
 
 let () = 
   let content = Some (File_key.SourceFile "sample.js") in
-  let result = program_file source content in
+  let (ast, _) = program_file source content in
   print_endline source;
 
